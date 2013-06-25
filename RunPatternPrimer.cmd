3749e8743f81b69fb6a38953554c7a9294e0e252
Rune Kobberø-Hansen <rkh@1508.dk>
2013-06-25 16:07:05 +0200

    Updated cmd's

--- RunPatternPrimer.cmd
+++ RunPatternPrimer.cmd
@@ -1,4 +1,6 @@
-@echo off
+cd..
+cp resources/css/styles.css Pattern-Primer/public
+cd pattern-primer
 CALL npm install connect
 start http://localhost:1508
 node pattern-primer.js
--- StartPatternPrimer.cmd
+++ StartPatternPrimer.cmd
@@ -1,1 +1,4 @@
+cd..
+cp resources/css/styles.css Pattern-Primer/public
+cd Pattern-Primer
 node pattern-primer.js
